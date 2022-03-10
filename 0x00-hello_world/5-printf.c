b9c5bb1cf8e9e005a23d57f45f4ff2f0ee73f70f
Dikachis <99288026+Dikachis@users.noreply.github.com>
2022-03-11 00:54:40 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,11 +5,8 @@
  * but the outcome is a piece of art.
  * Return: Always 0 (Success), else none zero
  */
-
 int main(void)
 { 
-	
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	
 	return (0);
 }
