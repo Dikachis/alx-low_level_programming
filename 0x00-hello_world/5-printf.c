6d9e0f7e70f44824fec96285b7cf049ef7ca6cca
Onyedikachi <dikachi015safety@gmail.com>
2022-03-10 15:16:41 -0800

    My C programme that printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints exactly the C programme with proper grammar, 
+ * but the outcome is a piece of art.
+ * Return: Always 0 (Success), else none zero
+ */
+
+ int main(void)
+ {
+
+	 printf("\"with proper grammar, but the outcome is a piece of art,");
+
+	 return (0);
+
+ }
