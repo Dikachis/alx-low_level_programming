7b53d7d559f60b194ee2a2cb550585646b7d3a18
Onyedikachi <dikachi015safety@gmail.com>
2022-03-10 14:16:11 -0800

    My 4-puts.C programme

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ *main - Entry point of the programme
+ *
+ *Return: Always 0 (Success), else none zero
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
