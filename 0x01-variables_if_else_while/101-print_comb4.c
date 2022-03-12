d181ecf0ca1ece143b8477d903ae30c746cb07b2
Dikachis <99288026+Dikachis@users.noreply.github.com>
2022-03-12 05:57:07 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,27 +7,27 @@
 
 int main(void)
 {
-        int d, p, q;
+	int d, p, q;
 
 	for (d = '0'; d < '9'; d++)
 	{
-		  for (p = d + 1; p <= '9'; p++)
-		  {
-			   for (q = p + 1; q <= '9'; q++)
-			   {
-			            if ((p != d) != q)
+		for (p = d + 1; p <= '9'; p++)
+		{
+			for (q = p + 1; q <= '9'; q++)
+			{
+				if ((p != d) != q)
 				    {
-					   putchar(d);
-					   putchar(p);
-					   putchar(q);
+					putchar(d);
+					putchar(p);
+					putchar(q);
 
-					   if (d == '7' && p == '8')
-						   continue;
+					if (d == '7' && p == '8')
+						continue;
 
-					   putchar(',');
-					   putchar(' ');
-				   }
-			 }
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
