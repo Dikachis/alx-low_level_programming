2167a7109c8a573ebda31e7f226f074cad58dbba
Dikachis <99288026+Dikachis@users.noreply.github.com>
2022-03-12 04:36:06 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,13 +13,13 @@ int main(void)
 	for (d = '0'; d <= '9'; d++)
 	{
 		putchar(d);
-	
+
 	if (d != '9')
-	{			
+	    {			
 		putchar(',');
-																			putchar(' ');															
+		putchar(' ');															
+	    }
 	}
-	
 	putchar('\n');
 
 	return (0);
