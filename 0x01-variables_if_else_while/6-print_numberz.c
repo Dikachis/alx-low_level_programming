03ca5e31e8a3efa2b4f87d51935593004330bb33
Onyedikachi <dikachi015safety@gmail.com>
2022-03-11 18:09:05 -0800

    Alphabet base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line, using putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; ++n)
+					
+		putchar(n);
+
+		putchar(10);
+
+	return (0);
+
+}
