833113a15cf8dea4bf686c6b4289289d2f73cb6e
Onyedikachi <dikachi015safety@gmail.com>
2022-03-11 19:01:11 -0800

    My base 16 alphanumeric

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	char ch;
+
+
+	for (n = '0'; n <= '9'; n++)
+	
+		putchar(n);
+	
+	for (ch = 'a'; ch <= 'f'; ch++)
+		
+		putchar(ch);
+		
+	putchar('\n');
+
+	return (0);
+}
