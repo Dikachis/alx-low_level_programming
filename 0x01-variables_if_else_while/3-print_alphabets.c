92eb8536f177e0c09fb8d613ccc882bf890db0c0
Dikachis <99288026+Dikachis@users.noreply.github.com>
2022-03-12 01:34:06 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,18 +8,18 @@
 
 int main(void)
 {
-	char ch = 'a';
-	char ch = 'A';
+
+	char ch
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 
-		putchar("%c", ch);
+		putchar(ch);
 
 	for (ch = 'A'; ch <= 'Z'; ch++)
 
-		putchar("%", ch);
+		putchar(ch);
 
 	putchar('\n');
-	
+
 	return (0);
 }
