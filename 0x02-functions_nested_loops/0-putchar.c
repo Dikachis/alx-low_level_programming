4abd7212310e0153fa26aa40b896dc9131ac1f8d
Onyedikachi <dikachi015safety@gmail.com>
2022-03-15 01:58:43 -0700

    My putchar commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - prints _putchar, followed by a new line.
+ * executes and prints on entry
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char string[8] = "_putchar";
+	int n = 0;
+
+	while (n < 8)
+	{
+		_putchar(string[n]);
+		++n;
+		}
+
+	_putchar(10);
+	return (0);
+}
