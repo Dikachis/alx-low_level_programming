104cc584ebb445294343ef2282783fb0e4cafed9
Onyedikachi <dikachi015safety@gmail.com>
2022-03-15 10:11:19 -0700

    6-abs commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - function to get the absolute value
+ * @n: parameter of the funtion
+ * Return: the absolute value
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-n);
+	}
+}
