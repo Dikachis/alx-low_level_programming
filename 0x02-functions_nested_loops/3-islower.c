0cc9f01f5419faa67a922f8033a5c97cbd3a3d35
Dikachis <99288026+Dikachis@users.noreply.github.com>
2022-03-15 17:07:22 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - 3-islower check
+ * _islower check main
  * @c: An input character
  * Description: function uses _putchar function to print
  * alphabet in lowercase 10 times
