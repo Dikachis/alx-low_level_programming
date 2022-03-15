2d39f96bfb59b332af004879059e933e1e8eebe9
Onyedikachi <dikachi015safety@gmail.com>
2022-03-15 10:22:08 -0700

    7-print commits

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit
+ * @n: parameter
+ * Return: return the last digit
+ */
+
+int print_last_digit(int n)
+{
+	int d = n % 10;
+
+	if (d < 0)
+	{
+		_putchar(-d + '0');
+		return (-d);
+	}
+	else
+	{
+		_putchar(d + '0');								return (d);
+	}
+}
+
