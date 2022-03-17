f8e1774c40598133faffd8b9fab55fb9e55c95ac
Onyedikachi <dikachi015safety@gmail.com>
2022-03-17 12:08:00 -0700

    Added 101-print_number.c file

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_number - a function that prints an integer.
+ * @n: An input integer
+ * Return: Nothing
+ */
+
+void print_number(int n)
+{
+	unsigned int n2, number, base10 = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n2 = -n;
+	}
+	else
+	{
+		n2 = n;
+	}
+	number = n2;
+	/* Obtenemos su base */
+	while (number > 9)
+	{
+		number = number / 10;
+		base10 = base10 * 10;
+	}
+	/* Descomponemos en base 10*/
+	number = n2;
+	while (base10 > 1)
+	{
+		_putchar((number / base10) + '0');
+		number = number % base10;
+		base10 = base10 / 10;
+	}
+		_putchar((n2 % 10) + '0');
+}
