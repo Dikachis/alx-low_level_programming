1844385e45660f9c12286c91c81811d3a759584d
Dikachis <dikachi015safety@gmail.com>
2022-03-21 18:15:55 +0100

    Added 1-swap.c file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - function
+ *
+ * @a: pointer of parameter
+ * @b: pointer of parameter
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = (*a);
+	(*a) = (*b);
+	(*b) = temp;
+}
