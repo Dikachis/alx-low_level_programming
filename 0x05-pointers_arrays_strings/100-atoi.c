a7550edda982e57f98710bd47b8511e3621b725f
Dikachis <dikachi015safety@gmail.com>
2022-03-21 19:31:49 +0100

    Update

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - a function that converts string to integer
+ * _atoi - a function that converts string to integer
  * @s: An input string
  * Return: integer from conversion
  */
