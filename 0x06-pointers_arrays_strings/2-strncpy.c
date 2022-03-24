ef34e41cfa8070608931357e8acb9b9c4abfd38f
Dikachis <99288026+Dikachis@users.noreply.github.com>
2022-03-24 19:13:55 +0100

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,38 +7,16 @@
  * @n: an input integer
  * Return: A pointer to the resulting string
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-<<<<<<< HEAD
 	int i;
-=======
-	int srclen = 0, i = 0;
-	char *temp = dest, *start = src;
->>>>>>> d99bcd6321e686c3b5ba87146485a848e6d42d79
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-<<<<<<< HEAD
 		dest[i] = src[i];
 	}
 	for (; i < n; i++)
 		dest[i] = '\0';
 	return (dest);
-=======
-		srclen++;
-		src++;
-	}
-
-	srclen++;
-
-	if (n > srclen)
-		n = srclen;
-
-	src = start;
-
-	for (; i < n; i++)
-		*dest++ = *src++;
-
-	return (temp);
->>>>>>> d99bcd6321e686c3b5ba87146485a848e6d42d79
 }
