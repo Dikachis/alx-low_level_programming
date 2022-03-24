88ff47e11cc577ab7adb1841109edccae86e54ea
Dikachis <99288026+Dikachis@users.noreply.github.com>
2022-03-24 22:21:16 +0100

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * infinite_add -  adds two numbers
  * @n1: first number
@@ -6,10 +7,9 @@
  * @r: result
  * @size_r: result lenght
  * Return: sum
- *
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	/* local variable declaration */
 	int i = 0, j = 0, k, l = 0, f, s, d = 0;
