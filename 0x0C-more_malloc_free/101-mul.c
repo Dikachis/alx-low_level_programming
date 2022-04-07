b3907c510aa58985ebdbf412ae7b172f38ae7839
Dikachis <dikachi015safety@gmail.com>
2022-04-07 16:01:51 +0000

    updated

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
+
 /**
 * main - multiply 2 numbers as arguments
 * @argc: number of arguments
 * @argv: arguments
 * Return: 0 success
 */
+
 int main(int argc, char *argv[])
 {
 unsigned long mul;
