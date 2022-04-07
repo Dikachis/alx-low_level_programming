5864bf0665a5b2693601870e648c13a8a3d80629
Dikachis <99288026+Dikachis@users.noreply.github.com>
2022-04-07 18:04:34 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,11 +9,13 @@
 */
 int main(int argc, char *argv[])
 {
-unsigned long mul;
-int num1, num2;
+	unsigned long mul;
+	int num1, num2;
 	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
+	{
+		printf("Error\n");
+		exit(98); 
+	}
 	for (num1 = 1; num1 < argc; num1++)
 	{
 		for (num2 = 0; argv[num1][num2] != '\0'; num2++)
