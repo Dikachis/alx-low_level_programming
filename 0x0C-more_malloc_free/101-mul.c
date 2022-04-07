6a634a40b28021c8d2b00b402beb78a2d4dfa939
Dikachis <99288026+Dikachis@users.noreply.github.com>
2022-04-07 22:24:40 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,26 +2,29 @@
 #include <stdlib.h>
 
 /**
- * main - Check the code
- * @argc: number of arguments
- * @argv: arguments
- * Return: Nothing
- */
-int main(int argc, char **argv)
+* main - multiply 2 numbers as arguments
+* @argc: number of arguments
+* @argv: arguments
+* Return: 0 success
+*/
+int main(int argc, char *argv[])
 {
-	int mul, num1, num2;
-
+unsigned long mul;
+int num1, num2;
 	if (argc != 3)
+	{ printf("Error\n");
+	exit(98); }
+	for (num1 = 1; num1 < argc; num1++)
 	{
-		printf("Error\n");
-		exit(98);
+		for (num2 = 0; argv[num1][num2] != '\0'; num2++)
+		{
+			if (argv[num1][num2] > 57 || argv[num1][num2] < 48)
+			{  printf("Error\n");
+			exit(98); }
+		}
+
 	}
-	else
-	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		mul = num1 * num2;
-		printf("%d\n", mul);
-		return (0);
-	}
+	mul = atol(argv[1]) * atol(argv[2]);
+	printf("%lu\n", mul);
+return (0);
 }
