3e9842eac34a58387c93f316108e82e0ec62f9e3
Dikachis <dikachi015safety@gmail.com>
2022-04-11 19:26:40 +0000

    updated 2-main.c file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file it was compiled from, followed by a new line.
+ * main - prints the name of the file it was compiled from
  *
  * Return: always 0
  */
