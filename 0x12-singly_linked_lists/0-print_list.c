606c47cfda3c404125bd07485a9185b4df5ba84a
Dikachis <dikachi015safety@gmail.com>
2022-04-22 00:28:33 +0100

    My latest commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,7 +4,7 @@
 /**
  * print_list - prints all the elements of a list_t list
  * @h: pointer to the start of the list
- * 
+ *
  * Return: number of nodes in the list
  */
 size_t print_list(const list_t *h)
