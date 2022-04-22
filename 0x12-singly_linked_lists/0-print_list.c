36929d6badb6f3d224845a6376a384f15138faa8
Dikachis <dikachi015safety@gmail.com>
2022-04-22 01:13:48 +0100

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,7 +15,10 @@ size_t print_list(const list_t *h)
 	tmp = h;
 	for (i = 0; tmp != NULL; i++)
 	{
-		printf("[%u] %s\n", tmp->len, tmp->str);
+		if (tmp->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", tmp->len, tmp->str);
 		tmp = tmp->next;
 	}
 	return (i);
