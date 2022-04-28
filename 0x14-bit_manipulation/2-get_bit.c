5d8e8046393857cb83a8201016086364eafd796b
Dikachis <dikachi015safety@gmail.com>
2022-04-28 22:18:37 +0000

    Added vi 2-get_bit.c and 3-set_bit.c files

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_bit - function with two arguments
+ * @n: value to compare
+ * @index: position to compare
+ *
+ * Description: returns the value of a bit at a given index
+ * Return: 0 or -1 if error
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > (sizeof(n) * 8))
+		return (-1);
+
+	n  = n >> index;
+	if (n & 1)
+		return (1);
+	else
+		return (0);
+}
