6fdf693aa9f820c8b6d3a4caff2fbd5b3a67d05c
Dikachis <dikachi015safety@gmail.com>
2022-04-28 22:09:36 +0000

    Added 1-print_binary.c file

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_binary - function with one argument
+ * @n: decimal value
+ *
+ * Description: prints the binary representation of a number
+ * Return: na
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long value = n;
+
+	if (value > 1)
+	{
+		print_binary(value >> 1);
+	}
+	_putchar((value & 1) + '0');
+}
