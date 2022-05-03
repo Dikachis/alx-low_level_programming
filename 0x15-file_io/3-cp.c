155d2df9dd1cceb647b18c2c8890586a55d86900
Dikachis <dikachi015safety@gmail.com>
2022-05-03 02:25:07 +0000

    file update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <unistd.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
-
 /**
  * main - function with two arguments
  * @argc: count of arguments
