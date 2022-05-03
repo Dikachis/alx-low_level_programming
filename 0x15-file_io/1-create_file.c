499313b746fdbad587557ebb8e6bc0ca950206d2
Dikachis <dikachi015safety@gmail.com>
2022-05-03 01:14:08 +0000

    Updated 1-create_file.c file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,6 +4,14 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+/**
+ * create_file - function with two arguments
+ * @filename: pointer to string name
+ * @text_content: pointer to string contents
+ *
+ * Description: creates a file
+ * Return: 1 on success or -1 failure
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int count = 0;
