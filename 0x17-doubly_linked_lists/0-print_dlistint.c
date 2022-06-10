f0d38b47413a45bebd4368f0922197e04102a23c
Dikachis <dikachi015safety@gmail.com>
2022-06-10 22:42:43 +0100

    updated the 0-print_

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,10 +9,9 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i = 0;
 	for (node* h = tail; h != NULL; h = h->next)
 	{
 		printf("%d\n", h->n);
 	}
-	return (i);
+	return (node* h = tail);
 }
