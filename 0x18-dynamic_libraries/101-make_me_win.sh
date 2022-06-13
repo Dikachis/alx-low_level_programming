69b351c4859065787e992e8737502feed4eb0c44
Dikachis <dikachi015safety@gmail.com>
2022-06-13 22:31:20 +0100

    file updated

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/Dikachis/alx-low_level_programming/main/0x18-dynamic_libraries/dikamil.so
-export LD_PRELOAD="$PWD/../dikamil.so"
+wget -P .. https://raw.githubusercontent.com/Dikachis/alx-low_level_programming/main/0x18-dynamic_libraries/dikamillion.so
+export LD_PRELOAD="$PWD/../dikamillion.so"
