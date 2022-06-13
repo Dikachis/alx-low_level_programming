b614c0b98558acf1ce8cdce9b16fbb19837b87f5
Dikachis <dikachi015safety@gmail.com>
2022-06-13 19:45:02 +0100

    Added 100-operations.so and 100-operations.h

---
+++ 0x18-dynamic_libraries/100-operations.h
@@ -0,0 +1,11 @@
+#ifndef _100_OPERATIONS_H_
+#define _100_OPERATIONS_H_
+
+int add_2_num(int num1, int num2);
+int sub_2_num(int num1, int num2);
+int mul_2_num(int num1, int num2);
+int pow_of_num(int num);
+int div_2_num(int num1, int num2);
+int mod_2_num(int num1, int num2);
+
+#endif /*_100_OPERATIONS_H_*/
