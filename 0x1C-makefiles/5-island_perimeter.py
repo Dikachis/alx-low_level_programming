800060b2df5aacc8adeb4957022ea365e0410a02
Dikachis <dikachi015safety@gmail.com>
2022-08-18 05:14:01 +0100

    Added grid perimeter file

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,14 +5,13 @@
 def island_perimeter(grid):
     """ Island Perimeter """
     peri = 0
-    grid_list = []
+    grl = []
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            grid_list.append(grid[i][j])
-    for i in range(len(grid_list)):
-        if i < len(grid_list) - 1 and i > 0:
-            if grid_list[i] == 0 and grid_list[i + 1] == 1 or
-            grid_list[i] == 0 and l[i - 1] == 1:
+            grl.append(grid[i][j])
+    for i in range(len(grl)):
+        if i < len(grl) - 1 and i > 0:
+            if grl[i] == 0 and grl[i + 1] == 1 or grl[i] == 0 and l[i - 1] == 1:
                 peri += 1
     if peri % 2 == 0:
         peri *= 2
